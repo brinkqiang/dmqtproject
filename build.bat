4f4834ebaf732fc11518306961cd78531d112625
brinkqiang <brink.qiang@gmail.com>
2022-05-03 22:42:06 +0800

    ADD

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 rmdir /S /Q build
 mkdir build
 pushd build
-cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo -DVCPKG_TARGET_TRIPLET=x64-windows ..
 cmake --build . --config relwithdebinfo
 
 popd
